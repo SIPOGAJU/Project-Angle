987a85ac57cc186efea820cae00f99d81f581749
Simon(Home Desktop) <Simon.schruff@gmail.com>
2020-11-21 22:53:58 +0100

    GameManager; Game_States + Singleton SetUP;

---
+++ PAF01-GROUP/Assets/Scripts/Managers/gameManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gameManager : MonoBehaviour
+{
+    
+    public static gameManager instance; 
+
+
+    public enum GAME_STATE
+    {
+        gameRunning,
+        gameOver,
+        gameFinished,
+        gamePaused
+    }
+    public GAME_STATE GameState;
+
+    private void Awake() 
+    {
+        if(instance == null) {
+            instance = this; 
+        }
+        else { 
+            Destroy(this.gameObject); 
+        }
+    }
+}
