a68257d86a77a204c11ac917bdb694859a0ee77e
Simon(Home Desktop) <Simon.schruff@gmail.com>
2020-11-22 17:02:33 +0100

    Buttons/Doors

---
+++ PAF01-GROUP/Assets/Scripts/Button.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button : MonoBehaviour
+{
+    [SerializeField] Vector3 doorTargetPos; 
+    Vector3 doorStartPosition; 
+    [SerializeField] GameObject door; 
+
+    bool doorIsOpen = false; 
+
+    private void Start()
+    {
+        doorStartPosition = door.transform.position; 
+    }
+
+
+    private void OnTriggerEnter(Collider c) 
+    {
+        if(c.CompareTag("Player"))
+        {
+            if(doorIsOpen == false)
+            {
+                door.transform.position = doorStartPosition + doorTargetPos; 
+                doorIsOpen = true; 
+            }
+            else if(doorIsOpen == true)
+            {
+                door.transform.position = doorStartPosition; 
+                doorIsOpen = false; 
+            }
+            
+
+        }
+
+    }
+
+
+}
