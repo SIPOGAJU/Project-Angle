67150fa507b1ed88772183b28dd908eddad027b3
IcaroSh <jccgsh@gmail.com>
2020-11-23 18:51:20 +0100

    Fixed camera script, fix basic collsion
    Collision working, I'll re adjust to have a smoother trasition to the parts the player should  not be able to move.
    
    Camera trasition working between two cameras, also looking for a smoother  transition for those

--- PAF01-GROUP/Assets/Scripts/CameraControls/CameraControl.cs
+++ PAF01-GROUP/Assets/Scripts/CameraControls/CameraControl.cs
@@ -1,39 +1,57 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 
 public class CameraControl : MonoBehaviour
 {
-    Quaternion _targetRot;
+    public CinemachineVirtualCamera playerCam;
+    public CinemachineVirtualCamera orbitCam;
+    
     [SerializeField] float rotationSpeed = 1f;
+    [SerializeField] float waitTime = 2f;
+    private float backToPlayerCam;
+
+    private Vector3 originalUp;
 
     // Start is called before the first frame update
     void Start()
     {
-        _targetRot = transform.rotation;
+        originalUp = transform.up;
     }
+
 
     // Update is called once per frame
     void Update()
     {
-        transform.rotation = Quaternion.Slerp(transform.rotation, _targetRot, rotationSpeed * Time.deltaTime);
-    }
+        float sideRotation = Input.GetAxisRaw("Horizontal") * rotationSpeed;
+        float upRotation = Input.GetAxisRaw("Vertical") * rotationSpeed;
 
-    public void RotateUp()
-    {
-        _targetRot *= Quaternion.AngleAxis(90, Vector3.right);
-    }
-    public void RotateDown()
-    {
-        _targetRot *= Quaternion.AngleAxis(-90, Vector3.right);
-    }
+        Debug.Log(sideRotation);
 
-    public void RotateLeft()
-    {
-        _targetRot *= Quaternion.AngleAxis(90, Vector3.up);
-    }
-    public void RotateRight()
-    {
-        _targetRot *= Quaternion.AngleAxis(-90, Vector3.up);
+        if (sideRotation != 0 && upRotation == 0)
+        {
+            transform.Rotate(0f, -sideRotation, 0f);
+            orbitCam.Priority = 11;
+            backToPlayerCam = waitTime;
+        }
+
+        else if(upRotation != 0 && sideRotation == 0)
+        {
+            transform.Rotate(upRotation, 0f, 0f);
+            orbitCam.Priority = 11;
+            backToPlayerCam = waitTime;
+        }
+
+        if(sideRotation == 0f && upRotation == 0f)
+        {
+            backToPlayerCam -= Time.deltaTime;
+
+            if(backToPlayerCam <= 0)
+            {
+                orbitCam.Priority = 5;
+                transform.up = originalUp;
+            }
+        }
     }
 }
