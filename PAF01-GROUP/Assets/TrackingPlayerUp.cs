4f8cb8fdcc921af73d1feb8df93eb051f48c26bd
IcaroSh <jccgsh@gmail.com>
2020-11-24 12:22:52 +0100

    Cinemachine updates

---
+++ PAF01-GROUP/Assets/TrackingPlayerUp.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrackingPlayerUp : MonoBehaviour
+{
+    public Transform playerTransform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.up = playerTransform.up;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
