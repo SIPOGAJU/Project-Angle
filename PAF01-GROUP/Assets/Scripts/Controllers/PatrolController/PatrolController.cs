5f72dae65f87048a219afb1be896646ce8214152
IcaroSh <jccgsh@gmail.com>
2020-11-24 17:05:27 +0100

    Patrol system changes

--- PAF01-GROUP/Assets/Scripts/Controllers/PatrolController/PatrolController.cs
+++ PAF01-GROUP/Assets/Scripts/Controllers/PatrolController/PatrolController.cs
@@ -6,7 +6,6 @@ public class PatrolController : MonoBehaviour
 {
     [SerializeField] float agentSpeed = 0f;
     public Transform[] wayPoints;
-    public ParticleSystem playerDeath;
 
     private int currentPoint = 0;
 
@@ -35,16 +34,6 @@ public class PatrolController : MonoBehaviour
 
         else if (currentPoint >= wayPoints.Length - 1)
             currentPoint -= 1;
-    }
-
-    private void OnCollisionEnter(Collision collision)
-    {
-        if (collision.gameObject.CompareTag("Player"))
-        {
-            Destroy(collision.gameObject);
-            Instantiate(playerDeath, collision.gameObject.transform.position, collision.gameObject.transform.rotation);
-            Debug.Log("You have been eliminated for goloso!");
-        }
     }
 
     private void OnTriggerEnter(Collider other)
