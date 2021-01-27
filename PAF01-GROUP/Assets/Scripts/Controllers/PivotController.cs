c95d257aecc3250e3baf77e5ad8ebf165579da5a
IcaroSh <jccgsh@gmail.com>
2021-01-27 20:06:04 +0100

    Updated and cleaned

---
+++ PAF01-GROUP/Assets/Scripts/Controllers/PivotController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class PivotController : MonoBehaviour
+{
+    [SerializeField] float timer = 3f;
+    private float waitTime;
+    void Start()
+    {
+        waitTime = 2f;
+    }
+
+    void Update()
+    {
+        if (Time.time > waitTime)
+        {
+            waitTime = Time.time + timer;
+            transform.DORotate(new Vector3(0, 0, 90), .6f, RotateMode.WorldAxisAdd).SetEase(Ease.OutBack);
+        }
+    }
+}
--- PAF01-GROUP/Assets/Scripts/Managers/ConditionManager.cs
+++ PAF01-GROUP/Assets/Scripts/Managers/ConditionManager.cs
@@ -21,15 +21,32 @@ public class ConditionManager : MonoBehaviour
         foreach (PathCondition pc in pathConditions)
         {
             int count = 0;
-            for (int i = 0; i < pc.conditions.Count; i++)
+
+            if(pc.usePosition)
             {
-                if (pc.conditions[i].conditionObject.position == pc.conditions[i].position)
+                for (int i = 0; i < pc.conditions.Count; i++)
                 {
-                    count++;
+                    if (pc.conditions[i].conditionObject.position == pc.conditions[i].position)
+                    {
+                        count++;
+                    }
                 }
+                foreach (SinglePath sp in pc.paths)
+                    sp.block.possiblePaths[sp.index].active = (count == pc.conditions.Count);
             }
-            foreach (SinglePath sp in pc.paths)
-                sp.block.possiblePaths[sp.index].active = (count == pc.conditions.Count);
+            
+            else if (pc.useRotation)
+            {
+                for (int i = 0; i < pc.conditions.Count; i++)
+                {
+                    if (pc.conditions[i].conditionObject.eulerAngles == pc.conditions[i].eulerAngle)
+                    {
+                        count++;
+                    }
+                }
+                foreach (SinglePath sp in pc.paths)
+                    sp.block.possiblePaths[sp.index].active = (count == pc.conditions.Count);
+            }
         }
 
         if (player.walking)
@@ -44,13 +61,17 @@ public class PathCondition
     public string pathConditionName;
     public List<Condition> conditions;
     public List<SinglePath> paths;
+
+
+    public bool usePosition;
+    public bool useRotation;
 }
 [System.Serializable]
 public class Condition
 {
     public Transform conditionObject;
     public Vector3 position;
-
+    public Vector3 eulerAngle;
 }
 [System.Serializable]
 public class SinglePath
