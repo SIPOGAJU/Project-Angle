e5e4171c843f3ca50a08e020c74c5e0b37e493e1
IcaroSh <jccgsh@gmail.com>
2021-01-29 09:34:53 +0100

    Audio

--- PAF01-GROUP/Assets/Scripts/Managers/AudioManager.cs
+++ PAF01-GROUP/Assets/Scripts/Managers/AudioManager.cs
@@ -30,7 +30,10 @@ public class AudioManager : MonoBehaviour
             s.source.loop = s.loop;
         }
     }
-
+    private void Start()
+    {
+        Play("Theme");
+    }
     public void Play(string name)
     {
        Sound s = Array.Find(sounds, sound => sound.name == name);
